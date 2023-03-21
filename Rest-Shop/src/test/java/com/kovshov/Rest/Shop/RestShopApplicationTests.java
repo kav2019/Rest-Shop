fd321ffff83a5c1471d255947d9cda4d6db0a353
Alexandr <Alexandr>
2023-03-21 18:46:51 +0300

    start

---
+++ Rest-Shop/src/test/java/com/kovshov/Rest/Shop/RestShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kovshov.Rest.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
